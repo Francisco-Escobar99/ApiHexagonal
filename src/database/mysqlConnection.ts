b32ac38d18e3c62bb41a18f9904845ee99de50f9
Francisco-Escobar99 <escobarfrancisco9905@gmail.com>
2023-07-30 20:30:45 -0600

    arqHexagonal1

--- src/database/mysqlConnection.ts
+++ src/database/mysqlConnection.ts
@@ -1,9 +1,8 @@
-// mysqlConnection.ts
 import { createConnection } from 'mysql2/promise';
 
 export const getConnection = async () => {
   return await createConnection({
-    host: '54.163.119.51',
+    host: '54.163.119.51',//ip
     user: 'franciscoescobar',
     password: '1234',
     database: 'farmacia'
